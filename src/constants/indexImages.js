74616c1051f3dc6063dc7f849f2ba65ca8d439a7
olena-petenok <olena.petenok.work@gmail.com>
2020-05-10 16:43:46 +0300

    index jsons are rewrittent

--- src/constants/indexImages.js
+++ src/constants/indexImages.js
@@ -1,17 +1,17 @@
-import BeforeOrdering1 from '../images/index/before-ordering/1.svg';
-import BeforeOrdering2 from '../images/index/before-ordering/2.svg';
-import BeforeOrdering3 from '../images/index/before-ordering/3.svg';
-import BeforeOrdering4 from '../images/index/before-ordering/4.svg';
-import BeforeOrdering5 from '../images/index/before-ordering/5.svg';
-import BeforeOrdering6 from '../images/index/before-ordering/6.svg';
+import ReactIsAwesome1 from '../images/index/react-is-awesome/1.svg';
+import ReactIsAwesome2 from '../images/index/react-is-awesome/2.svg';
+import ReactIsAwesome3 from '../images/index/react-is-awesome/3.svg';
+import ReactIsAwesome4 from '../images/index/react-is-awesome/4.svg';
+import ReactIsAwesome5 from '../images/index/react-is-awesome/5.svg';
+import ReactIsAwesome6 from '../images/index/react-is-awesome/6.svg';
 
 import HighQualityWebsite1 from '../images/index/high-quality-website/1.svg';
 import HighQualityWebsite2 from '../images/index/high-quality-website/2.svg';
 
-import Services1 from '../images/index/services/1.svg';
-import Services2 from '../images/index/services/2.svg';
-import Services3 from '../images/index/services/3.svg';
-import Services4 from '../images/index/services/4.svg';
+import WebDevelopment1 from '../images/index/web-development/1.svg';
+import WebDevelopment2 from '../images/index/web-development/2.svg';
+import WebDevelopment3 from '../images/index/web-development/3.svg';
+import WebDevelopment4 from '../images/index/web-development/4.svg';
 
 import TypesOfWebsites1 from '../images/index/types-of-websites/1.svg';
 import TypesOfWebsites2 from '../images/index/types-of-websites/2.svg';
@@ -21,18 +21,18 @@ import TypesOfWebsites5 from '../images/index/types-of-websites/5.svg';
 import TypesOfWebsites6 from '../images/index/types-of-websites/6.svg';
 
 export const indexImages = {
-  'BeforeOrdering1': BeforeOrdering1,
-  'BeforeOrdering2': BeforeOrdering2,
-  'BeforeOrdering3': BeforeOrdering3,
-  'BeforeOrdering4': BeforeOrdering4,
-  'BeforeOrdering5': BeforeOrdering5,
-  'BeforeOrdering6': BeforeOrdering6,
+  'ReactIsAwesome1': ReactIsAwesome1,
+  'ReactIsAwesome2': ReactIsAwesome2,
+  'ReactIsAwesome3': ReactIsAwesome3,
+  'ReactIsAwesome4': ReactIsAwesome4,
+  'ReactIsAwesome5': ReactIsAwesome5,
+  'ReactIsAwesome6': ReactIsAwesome6,
   'HighQualityWebsite1': HighQualityWebsite1,
   'HighQualityWebsite2': HighQualityWebsite2,
-  'Services1': Services1,
-  'Services2': Services2,
-  'Services3': Services3,
-  'Services4': Services4,
+  'WebDevelopment1': WebDevelopment1,
+  'WebDevelopment2': WebDevelopment2,
+  'WebDevelopment3': WebDevelopment3,
+  'WebDevelopment4': WebDevelopment4,
   'TypesOfWebsites1': TypesOfWebsites1,
   'TypesOfWebsites2': TypesOfWebsites2,
   'TypesOfWebsites3': TypesOfWebsites3,
