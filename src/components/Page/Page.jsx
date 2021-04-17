93c1488baa5d7a5fb5456218f3859cc72de87b12
Olena Petenok <olena.petenok.work@gmail.com>
2021-04-17 17:51:36 +0300

    index basic refactoring

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import PropTypes from "prop-types";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -8,32 +7,25 @@ import AboutAuthor from "../AboutAuthor/AboutAuthor";
 
 import DataDocumentTitles from "../../constants/json/SharedData/DataDocumentTitles.json";
 
-function Page(props) {
-  const { language, page } = props;
+const Page = ({ language = "en", page = "index" }) => {
   const titles = DataDocumentTitles;
 
   useEffect(() => {
-    document.title = `${
-      titles[page][language] || titles["default"]["default"]
-    }`;
+    document.title = `${titles[page][language]}`;
   }, [language, page]);
 
   const pages = {
     index: <Index language={language} />,
     aboutAuthor: <AboutAuthor language={language} />,
-    default: <Index language={"en"} />,
   };
 
   return (
     <>
       <Header language={language} page={page} />
-      {pages[page] || pages["default"]}
+      {pages[page]}
       <Footer language={language} />
     </>
   );
-}
-
-Page.propTypes = { language: PropTypes.string, page: PropTypes.string };
-Page.defaultProps = { language: "en", page: "index" };
+};
 
 export default Page;
